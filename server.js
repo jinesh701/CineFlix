6eea4af18753e87225539c81549018d2e411dbc8
Jinesh Patel <jinesh701@gmail.com>
2017-12-18 18:45:32 -0500

    added runServer and closeServer functions

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+
+app.use(morgan("common"));
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 8080);
+let server;
 
-module.exports = app;
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.log(err));
+}
+
+module.exports = { app, runServer, closeServer };

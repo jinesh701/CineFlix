77839d4c8a6a18f67a9edff27865fdb975b43966
Jinesh Patel <jinesh701@gmail.com>
2017-12-18 02:17:49 -0500

    added initial test

---
+++ test/initial-test.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../server.js");
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Index page", function() {
+  it("should exist", function() {
+    return chai
+      .request(app)
+      .get("/")
+      .then(function(res) {
+        res.should.have.status(200);
+      });
+  });
+});

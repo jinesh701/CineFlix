dfd9a033685fd69a66a64e312b874c132804d5c5
Jinesh Patel <jinesh701@gmail.com>
2017-12-18 19:25:51 -0500

    changed paths for html pages

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
 app.use(morgan("common"));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/search", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/search.html"));
+});
+
+app.get("/watchlist", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/watchlist.html"));
+});
+
+app.get("/watched", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/watched.html"));
+});
 
 let server;
 

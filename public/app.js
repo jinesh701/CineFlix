807fcb50b29b6ad2119dc2c7a8de97b2ef63c11b
Jinesh Patel <jinesh701@gmail.com>
2017-12-19 08:30:32 -0500

    updated displayMovieData function

--- public/app.js
+++ public/app.js
@@ -2,35 +2,35 @@ var MOCK_MOVIE_DATA = {
   movieData: [
     {
       id: 111111,
-      poster: "/movie1.jpg",
+      poster: "http://via.placeholder.com/150x200",
       overview:
-        "Movie description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "Movie description 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "Movie Title - 1",
-      release_date: "2007-1-1"
+      release_date: "2017-1-1"
     },
     {
       id: 222222,
-      poster: "/movie2.jpg",
+      poster: "http://via.placeholder.com/150x200",
       overview:
-        "Movie description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "Movie description 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "Movie Title - 2",
-      release_date: "2007-2-2"
+      release_date: "2017-2-2"
     },
     {
       id: 333333,
-      poster: "/movie3.jpg",
+      poster: "http://via.placeholder.com/150x200",
       overview:
-        "Movie description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "Movie description 3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "Movie Title - 3",
-      release_date: "2007-3-3"
+      release_date: "2017-3-3"
     },
     {
       id: 444444,
-      poster: "/movie4.jpg",
+      poster: "http://via.placeholder.com/150x200",
       overview:
-        "Movie description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "Movie description 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "Movie Title - 4",
-      release_date: "2007-4-4"
+      release_date: "2017-4-4"
     }
   ]
 };
@@ -43,7 +43,21 @@ function getMovieData(callbackFn) {
 
 function displayMovieData(data) {
   for (index in data.movieData) {
-    $("body").append(`<p>${data.movieData[index].title}</p>`);
+    $("body").append(`<div class="row">
+    <div class="col-3">
+        <img src="${data.movieData[index].poster}">
+    </div>
+    <div class="col-3">
+        <h3 class="movie-name">${data.movieData[index].title}</h3>
+        <p class="movie-release-date">${data.movieData[index].release_date}</p>
+    </div>
+    <div class="col-3">
+        <p class="movie-description">${data.movieData[index].overview}</p>
+    </div>
+    <div class="col-3">
+        <button type="button" class="watchlist-btn">Add to watchlist</button>
+    </div>
+</div>`);
   }
 }
 
@@ -61,33 +75,33 @@ var MOCK_TV_DATA = {
       id: 111111,
       poster: "/tv1.jpg",
       overview:
-        "TV description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "TV description 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "TV Title - 1",
-      release_date: "2007-1-1"
+      release_date: "2017-1-1"
     },
     {
       id: 222222,
       poster: "/tv2.jpg",
       overview:
-        "TV description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "TV description 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "TV Title - 2",
-      release_date: "2007-2-2"
+      release_date: "2017-2-2"
     },
     {
       id: 333333,
       poster: "/tv3.jpg",
       overview:
-        "TV description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "TV description 3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "TV Title - 3",
-      release_date: "2007-3-3"
+      release_date: "2017-3-3"
     },
     {
       id: 444444,
       poster: "/tv4.jpg",
       overview:
-        "TV description - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+        "TV description 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "TV Title - 4",
-      release_date: "2007-4-4"
+      release_date: "2017-4-4"
     }
   ]
 };

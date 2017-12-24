76775aed3868fdf639f8bc82e1c82a1a277cb468
Jinesh Patel <jinesh701@gmail.com>
2017-12-24 02:38:11 -0500

    added path to profile page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const router = express.Router();
 
+//Index page
 router.get("/", (req, res) => {
   res.render("index", { title: "CineFlix" });
+});
+
+//Profile page
+router.get("/profile/:username", (req, res) => {
+  res.render("profile", { username: req.params.username });
 });
 
 module.exports = router;

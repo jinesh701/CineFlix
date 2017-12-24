ff3ebc5abdabd06704e71c9b849d18a6cce16987
Jinesh Patel <jinesh701@gmail.com>
2017-12-23 21:29:48 -0500

    updated user model

--- models/user.js
+++ models/user.js
@@ -1,37 +1,34 @@
 "use script";
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
 const UserSchema = mongoose.Schema({
+  firstName: {
+    type: String
+  },
   username: {
     type: String,
-    required: true,
-    unique: true
+    required: true
   },
   password: {
     type: String,
     required: true
-  },
-  firstName: { type: String, default: "" }
+  }
 });
-
-UserSchema.methods.serialize = function() {
-  return {
-    username: this.username || "",
-    firstName: this.firstName || ""
-  };
-};
-
-UserSchema.methods.validatePassword = function(password) {
-  return bcrypt.compare(password, this.password);
-};
-
-UserSchema.statics.hashPassword = function(password) {
-  return bcrypt.hash(password, 10);
-};
 
 const User = mongoose.model("User", UserSchema);
 
-module.exports = { User };
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  const query = {
+    username: username
+  };
+  User.findOne(query, callback);
+};
+
+module.exports = User;

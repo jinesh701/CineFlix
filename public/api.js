c51a446469bc69841ae6a919de2a021abdcc53f0
Jinesh Patel <jinesh701@gmail.com>
2017-12-26 22:57:15 -0500

    added more mock data to test adding to watchlist

--- public/api.js
+++ public/api.js
@@ -15,12 +15,32 @@ function getMovieInfo(searchMovie, callback) {
 var MOCK_MOVIE_DATA = {
   movieData: [
     {
-      id: 111111,
       poster: "http://via.placeholder.com/150x200",
       overview:
         "Movie description 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
       title: "Movie Title - 1",
       release_date: "2017-1-1"
+    },
+    {
+      poster: "http://via.placeholder.com/150x200",
+      overview:
+        "Movie description 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+      title: "Movie Title - 2",
+      release_date: "2017-2-2"
+    },
+    {
+      poster: "http://via.placeholder.com/150x200",
+      overview:
+        "Movie description 3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+      title: "Movie Title - 3",
+      release_date: "2017-3-3"
+    },
+    {
+      poster: "http://via.placeholder.com/150x200",
+      overview:
+        "Movie description 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at",
+      title: "Movie Title - 4",
+      release_date: "2017-4-4"
     }
   ]
 };

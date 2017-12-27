01346697862d9b24912b5446b107bf9503cfae9e
Jinesh Patel <jinesh701@gmail.com>
2017-12-27 02:12:45 -0500

    added createdAt and updatedAt to model

--- models/movies.js
+++ models/movies.js
@@ -2,12 +2,16 @@
 
 const mongoose = require("mongoose");
 
-const movieSchema = mongoose.Schema({
-  poster: String,
-  overview: String,
-  title: String,
-  release_date: String
-});
+const movieSchema = mongoose.Schema(
+  {
+    poster: String,
+    overview: String,
+    title: String,
+    release_date: String,
+    watched: { type: Boolean, required: true, default: false }
+  },
+  { timestamps: true }
+);
 
 const Movie = mongoose.model("Movies", movieSchema);
 

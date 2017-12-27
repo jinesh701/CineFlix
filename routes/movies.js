023d25e8b5e6d4d92771f2bab6a57d2df0cb07e1
Jinesh Patel <jinesh701@gmail.com>
2017-12-27 02:13:32 -0500

    added PUT to mark movies as watched

--- routes/movies.js
+++ routes/movies.js
@@ -12,7 +12,7 @@ router.get("/search", ensureAuthenticated, (req, res) => {
 
 //Watchlist page
 router.get("/watchlist", ensureAuthenticated, (req, res) => {
-  Movie.find()
+  Movie.find({ watched: false })
     .then(movies => {
       res.render("watchlist", { movies, title: "Watchlist" });
     })
@@ -24,7 +24,14 @@ router.get("/watchlist", ensureAuthenticated, (req, res) => {
 
 //Watched page
 router.get("/watched", ensureAuthenticated, (req, res) => {
-  res.render("watched", { title: "Watched" });
+  Movie.find({ watched: true })
+    .then(movies => {
+      res.render("watched", { movies, title: "Watched" });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: "No movies found" });
+    });
 });
 
 function ensureAuthenticated(req, res, next) {
@@ -60,6 +67,37 @@ router.delete("/watchlist/:id", (req, res) => {
     .then(() => {
       req.flash("success", "Movie has been removed from your watchlist");
       res.status(200).send({ redirect: "/movies/watchlist" });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+//Mark a movie as watched
+router.put("/watchlist/:id", (req, res) => {
+  Movie.findByIdAndUpdate(req.params.id, {
+    watched: true
+  })
+    .then(() => {
+      req.flash("success", "Your movie has been added to your watched list");
+      res.status(200).send({ redirect: "/movies/watched" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
+
+//Remove a movie from watched list
+router.delete("/watched/:id", (req, res) => {
+  Movie.findByIdAndRemove(req.params.id)
+    .then(() => {
+      req.flash(
+        "success",
+        "Your movie has been removed from your watched list"
+      );
+      res.status(200).send({ redirect: "/movies/watched" });
     })
     .catch(err => {
       console.error(err);

ac921c5f03d05509a309a78bcdc118d815857da3
Jinesh Patel <jinesh701@gmail.com>
2017-12-30 04:35:58 -0500

    added flash function for user

--- server.js
+++ server.js
@@ -87,6 +87,7 @@ app.use(function(req, res, next) {
   res.locals.success = req.flash("success");
   res.locals.error_msg = req.flash("error_msg");
   res.locals.error = req.flash("error");
+  res.locals.user = req.user || null;
   next();
 });
 
